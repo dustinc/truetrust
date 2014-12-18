85a659d8331cb16ed04a7f1df35b5dbda15e19e4
Dustin <coffey.dustin@gmail.com>
2014-12-17 19:43:35 -0800

    Add billing.jade, update app to render billing statement and download pdf.

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var express = require('express'),
     stylus = require('stylus'),
     mongoose = require('mongoose'),
     formage = require('formage'),
+    _ = require('lodash'),
     models,
     db;
 
@@ -64,8 +65,8 @@ app.set('formage_view_path', path.normalize(__dirname + '/views/formage'));
 app.set('env', process.env.NODE_ENV || 'development');
 app.set('port', process.env.PORT || 3000);
 app.set('mongo', process.env.MONGOLAB_URI || 'mongodb://localhost/truetrust');
-app.set('views', path.normalize(__dirname + '/views'));
 app.set('view engine', 'jade');
+app.set('views', path.normalize(__dirname + '/views'));
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -86,20 +87,59 @@ if('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+
+// app routes
 app.get('/', routes.index);
+
 
 // connect to mongodb
 db = mongoose.connect(app.get('mongo'));
 
 
-http.createServer(app).listen(app.get('port'), function() {
-  var admin = formage.init(app, express, models, {
-    title: 'True Trust Services',
-    root: '/admin',
-    admin_users_gui: true
+// init formage admin app
+var admin = formage.init(app, express, models, {
+  title: 'True Trust Services',
+  root: '/admin',
+  admin_users_gui: true
+});
+
+admin.app.locals.global_head = '<style>\n.formage .nf_listfield_container > .field_label { font-size: inherit; font-weight: normal; }\n</style>';
+
+
+admin.app.get('/model/:modelName/document/:documentId/statement', admin.app.controllers.authMiddleware('view'), function(req, res, next) {
+  // get customer
+  models.customer.findOne({ _id: req.params.documentId }, function(err, customer) {
+    if(err) return next(err);
+
+    // get payments
+    models.customer_payment.find({ customer: req.params.documentId }).exec(function(err, payments) {
+      var moment = require('moment');
+
+      // render billing statement html
+      admin.app.render('billing', { customer: customer, payments: payments, today_date: moment().format('MM/DD/YYYY'),  accounting: require('accounting') }, function(err, html) {
+        var fs = require('fs'),
+            pdf = require('html-pdf'),
+            file_name = customer.customer_id+'-001.pdf';
+        if(err) return next(err);
+
+        // html to pdf
+        pdf.create(html, {}, function(err, buffer) {
+          if(err) return next(err);
+
+          // write pdf to public dir
+          fs.writeFile('./public/'+file_name, buffer, function(err) {
+            if(err) return next(err);
+
+            // download
+            return res.redirect(req.protocol+'://'+req.get('host')+'/'+file_name);
+          });
+        });
+      });
+    });
   });
+});
 
-  admin.app.locals.global_head = '<style>\n.formage .nf_listfield_container > .field_label { font-size: inherit; font-weight: normal; }\n</style>';
 
+http.createServer(app).listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
